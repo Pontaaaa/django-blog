bd5d1d0e50dacc8d7fd349d44b70a2d9a8b8c579
Pontaaaa <>
2023-12-25 22:53:29 +0000

    readies code for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!*ey8ins5tlu8o4l#zc=erdquvoy28+zo&^08u@c@s=wcn(l15
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [, '.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
